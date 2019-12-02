b0c29b222a509b9208632a1106462061df4b7dbe
Dillon Lees <ddlees@us.ibm.com>
2019-12-02 18:52:15 -0500

    build: include completion files in release

---
+++ build.rs
@@ -0,0 +1,21 @@
+include!("src/cli.rs");
+
+use std::path::PathBuf;
+
+fn main() {
+    let name = std::env::var("CARGO_PKG_NAME").unwrap();
+    let _target = std::env::var("TARGET").unwrap();
+    let manifest = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let profile = std::env::var("PROFILE").unwrap();
+
+    let outdir = PathBuf::from(manifest)
+        .join("target")
+        .join(profile);
+
+    Cli::clap().gen_completions(&name, Shell::Bash, &outdir);
+    Cli::clap().gen_completions(&name, Shell::Zsh, &outdir);
+    Cli::clap().gen_completions(&name, Shell::Fish, &outdir);
+    Cli::clap().gen_completions(&name, Shell::Elvish, &outdir);
+    Cli::clap().gen_completions(&name, Shell::PowerShell, &outdir);
+
+}
