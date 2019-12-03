00425b57042235912f58ccea4209c7118cd7c1e8
Dillon Lees <ddlees@us.ibm.com>
2019-12-03 11:00:41 -0500

    style: format and remove unnecessary use of Regex struct

--- build.rs
+++ build.rs
@@ -8,14 +8,11 @@ fn main() {
     let manifest = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     let profile = std::env::var("PROFILE").unwrap();
 
-    let outdir = PathBuf::from(manifest)
-        .join("target")
-        .join(profile);
+    let outdir = PathBuf::from(manifest).join("target").join(profile);
 
     Cli::clap().gen_completions(&name, Shell::Bash, &outdir);
     Cli::clap().gen_completions(&name, Shell::Zsh, &outdir);
     Cli::clap().gen_completions(&name, Shell::Fish, &outdir);
     Cli::clap().gen_completions(&name, Shell::Elvish, &outdir);
     Cli::clap().gen_completions(&name, Shell::PowerShell, &outdir);
-
 }
